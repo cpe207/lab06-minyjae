09fe262bbd9ea762926298de6b0c2a71d155a115
minyjae <jiradate_ora@cmu.ac.th>
2024-07-08 20:18:54 +0700

    complete law mung

--- q1.ts
+++ q1.ts
@@ -1,10 +1,21 @@
 // const axios = require("axios");
 import axios from "axios";
 
-/* assign interface/type to the function definition properly */
+  /* assign interface/type to the function definition properly */
+   interface userIdentity {
+     name : string;
+   }
+
 const getUser = async (userId: number) => {
-  /* Your code here */
-};
+  try {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`); // สร้าง res มารับ api จากเว็บตาม userId
+      const user = res.data; // สร้าง user มารับ resposed พร้อม data ออกมา
+      const output : userIdentity = user.name; // สร้าง output ที่ output เป็น interface userIdentity มารับ name
+      return output;
+    }catch (err) {
+    return "INVALID USER ID";
+  };
+}
 
 //test case
 const input1 = 1;
--- q2.ts
+++ q2.ts
@@ -2,8 +2,36 @@
 import axios from "axios";
 
 /* assign interface/type to the function definition properly */
+interface userIdentity {
+  name : string;
+}
+
+interface TODO {
+  owner: string;
+  title: string;
+  completed: boolean;
+}
+
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+      const todo = res.data; // รับ data จาก todo มา ซึ่งมี userId อยู่ในนั้นด้วย
+
+
+      const ownerTODO = await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`);
+      const owner : userIdentity = ownerTODO.data; // จึงเอา userId จาก todo มาหาข้อมูลคน แล้ว set owner ให้เป็น interface ตาม q1 พร้อมดึง data ออกมา
+
+
+      const output : TODO = {
+        owner: owner.name, // ดึงเอาเฉพาะ name มาโชว์
+        title: todo.title,
+        completed: todo.completed,
+      };
+      return output;
+  }catch (err) {
+      return "INVALID TODO ID"
+  }
+
 };
 
 //test case
